cf034d35edd4b0eb842ab86724cf317344b85264
jamesliuilsemaj@gmail.com <jamesliuilsemaj@gmail.com@7fb08cb7-80d6-0aed-b8bb-c48e990d9af6>
2012-05-20 00:15:07 +0000

    Can now drop multiple models on the map. Press 1 when current selected model is dropped to get a new model to place somewhere else. Can not delete models from map yet.

--- TexturedQuad_4_0/TexturedQuadWindows/TexturedQuadWindows/CustomModel.cs
+++ TexturedQuad_4_0/TexturedQuadWindows/TexturedQuadWindows/CustomModel.cs
@@ -10,25 +10,85 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 
-namespace RPGLibrary
+namespace TexturedQuadWindows
 {
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class CustomModel : Microsoft.Xna.Framework.GameComponent
+    public class CustomModel : DrawableGameComponent
     {
-        Vector3 position;
-        Model model;
-        Game currentGame;
-        public CustomModel(Game game, String assetName)
-            : base(game)
+        Model themodel;
+
+        public bool dropped = false;
+
+        public Point tileClicked;
+
+        //will be eventually read in from properties file as height the object will be drawn at in y axis (i.e. up)
+        public float heightabove = 2;
+        private Game1 thisGame;
+        //constructor will eventually need file name of mesh model to be given as one of the inputs
+        public CustomModel(Game1 g, Vector3 position, Model ourmodel)   
+            : base(g)
         {
-            // TODO: Construct any child components here
-            position = Vector3.Zero;
-            currentGame = game;
-            model = game.Content.Load<Model>(assetName);
+            Position = position;
+            thisGame = g;
+            themodel = ourmodel;
+           
         }
 
+        public Vector3 Position { get; set; }
+
+        public void Draw(GameTime gameTime, GraphicsDeviceManager graphics)
+        {
+            var _game = Game as Game1;
+
+            Matrix[] transforms = new Matrix[themodel.Bones.Count];
+            float aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+            themodel.CopyAbsoluteBoneTransformsTo(transforms);
+            Matrix projection = _game.gameCamera.Projection;
+            Matrix view = _game.gameCamera.ViewMatrix;
+
+            Vector3 tpos;//position to translate to
+
+            if (dropped == false)
+            {
+                tpos = _game.pointToput;
+            }
+            else
+            {
+                tpos = Position;
+            }
+
+
+
+            foreach (ModelMesh mesh in themodel.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.View = view;
+                    effect.Projection = projection;
+                    effect.World = transforms[mesh.ParentBone.Index]
+                        * Matrix.CreateScale(0.5f, 0.5f, 0.5f)
+                        * Matrix.CreateRotationX(MathHelper.ToRadians(-90))
+                        * Matrix.CreateTranslation(tpos)
+                        ;
+                }
+                mesh.Draw();
+            }
+
+
+            base.Draw(gameTime);
+        }
+
+
+        protected override void LoadContent()
+        {
+            // model file name needs to be read in to be given as input here. 
+
+            //
+            base.LoadContent();
+        }
         /// <summary>
         /// Allows the game component to perform any initialization it needs to before starting
         /// to run.  This is where it can query for any required services and load content.
@@ -40,16 +100,20 @@ namespace RPGLibrary
             base.Initialize();
         }
 
+
+
+
         /// <summary>
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
+
         public override void Update(GameTime gameTime)
         {
-            // TODO: Add your update code here
+
 
             base.Update(gameTime);
         }
-        
     }
 }
