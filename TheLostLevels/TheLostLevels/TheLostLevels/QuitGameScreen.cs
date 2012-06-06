0bf8ab19e44463a81a9a2c09622e588489792536
bachelorsgroveband <bachelorsgroveband@7fb08cb7-80d6-0aed-b8bb-c48e990d9af6>
2012-06-06 23:51:39 +0000

    Added but did not implement start screens

---
+++ TheLostLevels/TheLostLevels/TheLostLevels/QuitGameScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using New2DRPG.CoreComponents;
+
+
+    class QuitGameScreen : GameScreen
+    {
+        ButtonMenu menu;
+        Texture2D image;
+        Texture2D buttonImage;
+        SpriteFont spriteFont;
+        Vector2 imagePosition;
+
+        public QuitGameScreen(Game game)
+            : base(game)
+        {
+            LoadContent();
+
+            Components.Add(new BackgroundComponent(game, image, false));
+
+            imagePosition = new Vector2();
+            imagePosition.X = (game.Window.ClientBounds.Width - image.Width) / 2;
+            imagePosition.Y = (game.Window.ClientBounds.Height - image.Height) / 2;
+
+            string[] items = { "QUIT", "CANCEL" };
+            menu = new ButtonMenu(game, spriteFont, buttonImage);
+            menu.SetMenuItems(items);
+            Components.Add(menu);
+        }
+
+        public int SelectedIndex
+        {
+            get { return menu.SelectedIndex; }
+        }
+
+        protected override void LoadContent()
+        {
+            image = Content.Load<Texture2D>();//background image
+            buttonImage = Content.Load<Texture2D>();//button image
+            spriteFont = Content.Load<SpriteFont>();//font
+            base.LoadContent();
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            menu.Position = new Vector2((image.Width -
+                              menu.Width) / 2 + imagePosition.X,
+                              image.Height - menu.Height - 10 + imagePosition.Y);
+        }
+    }
+
---
+++ TheLostLevels/TheLostLevels/TheLostLevels/StartScreen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using New2DRPG.CoreComponents;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+    class StartScreen : GameScreen
+    {
+        ButtonMenu buttonMenu;
+        SpriteFont spriteFont;
+        Texture2D background;
+        Texture2D buttonImage;
+
+        public StartScreen(Game game) 
+            : base(game)
+        {
+            LoadContent();
+            Components.Add(new BackgroundComponent(game, background, true));
+
+            string[] items = { "START", 
+                                 "STOP", 
+                                 "HELP",  
+                                 "QUIT" };
+
+            buttonMenu = new ButtonMenu(
+                game,
+                spriteFont,
+                buttonImage);
+
+            buttonMenu.SetMenuItems(items);
+            Components.Add(buttonMenu);
+        }
+
+        public int SelectedIndex
+        {
+            get { return buttonMenu.SelectedIndex; }
+        }
+
+        protected override void LoadContent()
+        {
+            background = Content.Load<Texture2D>();//insert file path
+            buttonImage = Content.Load<Texture2D>(); //image 
+            spriteFont = Content.Load<SpriteFont>();//font
+            base.LoadContent();
+        }
+        public override void Show()
+        {
+            buttonMenu.Position = new Vector2((Game.Window.ClientBounds.Width -
+                                        buttonMenu.Width) / 2, 450);
+            base.Show();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+        }
+    }
